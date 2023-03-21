82ade497345cbd97a2681c5ebff6292259690066
sreejyothi <sreejyothi@qburst.com>
2023-03-21 12:22:04 +0530

    added

---
+++ src/test/java/helixTest/HelixTest.java
@@ -0,0 +1,32 @@
+package helixTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class HelixTest {
+	WebDriver driver;
+
+   
+
+    @BeforeTest
+    public void setUp() {
+        this.driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void Login_to_the_application_with_valid_credentials() {
+        this.driver.get("https://www.saucedemo.com/");
+        this.driver.findElement(By.id("user-name")).sendKeys(new CharSequence[]{"standard_user"});
+        this.driver.findElement(By.id("password")).sendKeys(new CharSequence[]{"secret_sauce"});
+        this.driver.findElement(By.id("login-button")).click();
+    }
+
+    @AfterTest
+    public void tearDown() {
+        this.driver.quit();
+    }
+}
